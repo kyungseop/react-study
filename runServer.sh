d774e44a954e6e36e047fb639ab095427a46c393
kyungseop <kyungseop@thebettertech.co.kr>
2018-10-01 01:43:33 +0900

    * http-server 실행 쉘 추가 * chapter README 문서 추가

--- runServer.sh
+++ runServer.sh
@@ -1,1 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/bash
+#http-server 실행
+# npm install -g http-server  <= 사전 설치 필요
+
+if [ -z $1 ];then
+    echo "Usage: ./runServer.sh [index.html 경로]"
+    echo "Usage: ./runServer.sh ch03/clock"
+    exit 1;
+fi
+
+http-server $1
